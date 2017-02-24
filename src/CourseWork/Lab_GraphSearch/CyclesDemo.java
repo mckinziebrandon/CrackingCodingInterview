2b7368df5e237aeba33e2982a00b93dd4f676897
mckinziebrandon <mckinziebrandon@berkeley.edu>
2017-02-23 16:29:38 -0800

    Taking a break from cycles to work on next section.

--- src/CourseWork/Lab_GraphSearch/CyclesDemo.java
+++ src/CourseWork/Lab_GraphSearch/CyclesDemo.java
@@ -7,8 +7,9 @@ package CourseWork.Lab_GraphSearch;
 public class CyclesDemo {
     /* Identifies a cycle (if any exist) in the given graph, and draws the cycle with
      * a purple line. */
+    public static final String CONFIG_PATH = "/home/brandon/Documents/CrackingCodingInterview/src/CourseWork/Lab_GraphSearch/maze.config";
     public static void main(String[] args) {
-        Maze maze = new Maze("maze.config");
+        Maze maze = new Maze(CONFIG_PATH);
 
         MazeCycles mc = new MazeCycles(maze);
         mc.solve();
--- src/CourseWork/Lab_GraphSearch/MazeCycles.java
+++ src/CourseWork/Lab_GraphSearch/MazeCycles.java
@@ -1,5 +1,8 @@
 package CourseWork.Lab_GraphSearch;
 
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.ArrayList;
 import java.util.Observable;
 /** 
  *  @author Josh Hug
@@ -12,12 +15,56 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int start;      /* Label of starting vertex. */
+    private Queue<Integer> fringe;
+    private ArrayList<Integer>[] pathOf;
+
     public MazeCycles(Maze m) {
+         // Creates & initializes the inherited integer arrays.
         super(m);
+        this.start  = maze.xyTo1D(1, 1); // assuming that maze is connected.
+        this.distTo[start] = 0;
+        this.edgeTo[start] = start;
+        this.pathOf = (ArrayList<Integer>[]) new ArrayList[maze.V()];
+        fringe = new Queue<>();
     }
 
     @Override
     public void solve() {
+
+
+        // 1. Initialize fringe with starting vertex v;
+        assert(fringe.isEmpty());
+        fringe.enqueue(start);
+        marked[start] = true;
+
+        int prevNode = start;
+        while (!fringe.isEmpty()) {
+
+            int w = fringe.dequeue();
+            //ihavenoideawhatimdoing
+            //this.pathOf[prevNode].add(w);
+            marked[w] = true;
+            announce();
+
+            for (int adjToW : maze.adj(w)) {
+                if (adjToW == prevNode) continue;
+
+                if (!marked[adjToW]) fringe.enqueue(adjToW);
+                else identifyCycle(adjToW);
+                announce();
+                distTo[adjToW] = distTo[w] + 1;
+            }
+
+            prevNode = w;
+        }
+
+        System.out.println("chill");
+    }
+
+    private void identifyCycle(int v) {
+        //announce();
+        //ihavenoideawhatimdoing
     }
 } 
 
