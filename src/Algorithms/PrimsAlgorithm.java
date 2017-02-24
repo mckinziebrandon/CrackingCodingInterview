4f122893fac25baa7f11d44bfa35add86a37b421
mckinziebrandon <mckinziebrandon@berkeley.edu>
2017-02-23 17:58:02 -0800

    I don't remember what I did.

---
+++ src/Algorithms/PrimsAlgorithm.java
@@ -0,0 +1,25 @@
+package Algorithms;
+
+import edu.princeton.cs.algs4.Queue;
+import DataStructures.Interfaces.Graph;
+
+/**
+ * Procedure for finding the MST, given some undirected graph G.
+ */
+public class PrimsAlgorithm {
+    private class Node {
+        private int label, weight;
+    }
+
+    /** Return the MST (still technically a graph...) corresponding to graph G. */
+    public static Graph getMST(Graph G) {
+
+        // Wow, such progress!
+        Queue<Node> fringe = new Queue<>();
+
+
+
+    }
+
+
+}
